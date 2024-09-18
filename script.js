b06bc54b51320042aa394a033cb100843db59c24
Christopher <voschristopher53@gmail.com>
2024-09-18 22:22:49 +0200

    updated script to be more in line with the functional paradigm

--- script.js
+++ script.js
@@ -1,68 +1,80 @@
 import { reducer } from "./reducer.js"
 import { createStore }  from "./store.js"
 
-const initialState = {
-    counter: 0
+// Pure function to get DOM elements
+const getDomElements = () => ({
+    counter: document.getElementById("counter"),
+    addBtn: document.getElementById("add"),
+    subtractBtn: document.getElementById("subtract"),
+    resetBtn: document.getElementById("reset"),
+    removeSubscribeBtn: document.getElementById("remove-subscriber")
+});
+
+// Pure function to create action creators
+const createActionCreators = () => ({
+    add: () => ({ type: 'ADD' }),
+    subtract: () => ({ type: 'SUBTRACT' }),
+    reset: () => ({ type: 'RESET' })
+});
+
+// Pure function to log state
+const logState = (message, getState) => {
+    console.log(message);
+    console.log(`state from store: ${getState().counter}`);
+    console.log("");
+};
+
+const runScenarios = (store) => {
+    const { getState, dispatch } = store;
+    const actions = createActionCreators();
+
+    logState("SCENARIO 1: Initial State Verification", getState);
+
+    logState("SCENARIO 2: Incrementing the Counter", getState);
+    dispatch(actions.add());
+    dispatch(actions.add());
+    logState("After incrementing twice", getState);
+    
+    logState("SCENARIO 3: Decrementing the Counter", getState);
+    dispatch(actions.subtract());
+    logState("After decrementing once", getState);
+
+    logState("SCENARIO 4: Resetting the Counter", getState);
+    dispatch(actions.reset());
+    logState("After resetting", getState);
 }
 
-const store = createStore(initialState, reducer);
 
-const tags = {
-    counter : document.getElementById("counter"),
-    addBtn : document.getElementById("add"),
-    subtractBtn : document.getElementById("subtract"),
-    resetBtn : document.getElementById("reset"),
-    removeSubscribeBtn : document.getElementById("remove-subscriber")
+const updateCounterTag = (value, element) => {
+    element.textContent = value;
 }
 
-console.log("SCENARIO 1: Initial State Verification");
-console.log("GETS STATE");
-console.log(`initial state is :${initialState.counter}`);
-console.log(`state from store is :${store.getState().counter}`);
+const setupEventListener = (element, event, action) => {
+    element.addEventListener(event, ()=> action());
+}
 
-console.log("");
+function main(){
+    const { 
+        counter, 
+        addBtn, 
+        subtractBtn, 
+        resetBtn, 
+        removeSubscribeBtn 
+    } = getDomElements();
+    const actions = createActionCreators();
 
-console.log("SCENARIO 2: Incrementing the Counter");
-console.log("Dispatch ADD once");
-store.dispatch({ type: 'ADD' });
-console.log("Dispatch ADD again state counter should be 2 now");
-store.dispatch({ type: 'ADD' });
-console.log(`state from store: ${store.getState().counter}`);
+    const initialState = { counter: 0 }
 
-console.log("");
+    const store = createStore(initialState, reducer);
 
-console.log("SCENARIO 3: Decrementing the Counter");
-console.log(`state from store: ${store.getState().counter}`);
-console.log("Dispatch SUBTRACT once state counter should be 1");
-store.dispatch({type: "SUBTRACT"});
-console.log(`state from store: ${store.getState().counter}`);
+    runScenarios(store);
+    
+    const unSubscribe = store.subscribe((state)=> updateCounterTag(state.counter, counter));
 
-console.log("");
+    setupEventListener(addBtn, "click", ()=> store.dispatch(actions.add()));
+    setupEventListener(subtractBtn, "click", ()=> store.dispatch(actions.subtract()));
+    setupEventListener(resetBtn, "click", ()=> store.dispatch(actions.reset()));
+    setupEventListener(removeSubscribeBtn, "click", ()=> unSubscribe());
+}
 
-console.log("SCENARIO 4: Resetting the Counter");
-console.log(`state from store: ${store.getState().counter}`);
-console.log("Dispatch RESET counter should be 0");
-store.dispatch({ type: "RESET" });
-console.log(`state from store: ${store.getState().counter}`);
-
-
-const unSubscriber = store.subscribe(function(state) {
-    console.log("counter tag subscriber function trigger at update");
-    tags.counter.textContent = state.counter;
-})
-
-tags.addBtn.addEventListener("click", ()=> {
-    store.dispatch({ type: 'ADD' });
-});
-
-tags.subtractBtn.addEventListener("click", ()=> {
-    store.dispatch({ type: 'SUBTRACT' });
-});
-
-tags.resetBtn.addEventListener("click", ()=> {
-    store.dispatch({ type: 'RESET' });
-});
-
-tags.removeSubscribeBtn.addEventListener("click", ()=> {
-    unSubscriber();
-});
+main();
