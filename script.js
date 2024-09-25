6713312420eab7ea52b11c773068eadbbdc7268e
Christopher <voschristopher53@gmail.com>
2024-09-25 19:23:55 +0200

    changed logs to be more readable

--- script.js
+++ script.js
@@ -11,9 +11,8 @@ const createActionCreators = () => ({
 
 // Pure function to log state
 const logState = (message, getState) => {
-    console.log(message);
-    console.log(`state from store: ${getState().counter}`);
-    console.log("");
+    // console.log(message);
+    console.log(`${message} state from store: ${getState().counter}`);
 };
 
 // Pure function to run scenarios
@@ -23,14 +22,20 @@ const runScenarios = (store) => {
 
     logState("SCENARIO 1: Initial State Verification", getState);
 
+    console.log("");
+
     logState("SCENARIO 2: Incrementing the Counter", getState);
     dispatch(actions.add());// uses the action defined in function inside the dispatch function
     dispatch(actions.add());// uses the action defined in function inside the dispatch
     logState("After incrementing twice", getState);
     
+    console.log("");
+    
     logState("SCENARIO 3: Decrementing the Counter", getState);
     dispatch(actions.subtract());// uses the action defined in function inside the dispatch function
     logState("After decrementing once", getState);
+    
+    console.log("");
 
     logState("SCENARIO 4: Resetting the Counter", getState);
     dispatch(actions.reset());// uses the action defined in function inside the dispatch function
@@ -51,6 +56,8 @@ function main(){
 
     // unsubscribing when we are done
     unSub();
+    
+    console.log("");
     console.log("unsubbed ");
     
     store.dispatch({type: "ADD"})
