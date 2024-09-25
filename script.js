35f4da53131d2415cacd386b3bc029afd25f81b9
Christopher <voschristopher53@gmail.com>
2024-09-25 18:35:59 +0200

    created a brand wih old submittion

--- script.js
+++ script.js
@@ -19,45 +19,79 @@ const logState = (message, getState) => {
 // Pure function to run scenarios
 const runScenarios = (store) => {
     const { getState, dispatch } = store;
-    const actions = createActionCreators();// creates the predefined actions!!!!!!
+    const actions = createActionCreators();
 
     logState("SCENARIO 1: Initial State Verification", getState);
 
     logState("SCENARIO 2: Incrementing the Counter", getState);
-    dispatch(actions.add());// uses the action defined in function inside the dispatch function
-    dispatch(actions.add());// uses the action defined in function inside the dispatch
+    dispatch(actions.add());
+    dispatch(actions.add());
     logState("After incrementing twice", getState);
     
     logState("SCENARIO 3: Decrementing the Counter", getState);
-    dispatch(actions.subtract());// uses the action defined in function inside the dispatch function
+    dispatch(actions.subtract());
     logState("After decrementing once", getState);
 
     logState("SCENARIO 4: Resetting the Counter", getState);
-    dispatch(actions.reset());// uses the action defined in function inside the dispatch function
+    dispatch(actions.reset());
     logState("After resetting", getState);
 }
 
+// Extras ----------------------------------------------------------------
+
+// update the counter tag
+const updateCounterTag = (value, element) => {
+    element.textContent = value;
+}
+
+/**
+ * 
+ * @param {HTMLElement} element the element to attach the eventlistener to
+ * @param {string} event the type of event
+ * @param {()=>{}} action function to run when the event is fired
+ * @returns a function to remove said event from the event list
+ */
+const setupEventListener = (element, event, action) => {
+    element.addEventListener(event, ()=> action());
+    return () => {
+        element.removeEventListener(event, action);
+    }
+}
+
+
+// Pure function to get DOM elements
+const getDomElements = () => ({
+    counter: document.getElementById("counter"),
+    addBtn: document.getElementById("add"),
+    subtractBtn: document.getElementById("subtract"),
+    resetBtn: document.getElementById("reset"),
+    removeSubscribeBtn: document.getElementById("remove-subscriber")
+});
 
 // main ----------------------------------------------------------------
 function main(){
     const initialState = { counter: 0 }
     const store = createStore(initialState, reducer);
 
-    const unSub = store.subscribe((state)=> {
-        console.log("subscriber state updated:", state);
-    })
+    runScenarios(store);
 
-    runScenarios(store);/// runs the scenarios!!!!
+    // Extras ----------------------------------------------------------------
+    
+    const { 
+        counter, 
+        addBtn, 
+        subtractBtn, 
+        resetBtn, 
+        removeSubscribeBtn 
+    } = getDomElements();
+    const actions = createActionCreators();
+    
+    const unSubscribe = store.subscribe((state)=> updateCounterTag(state.counter, counter));
 
-    // unsubscribing when we are done
-    unSub();
-    console.log("unsubbed ");
-    
-    store.dispatch({type: "ADD"})
-    console.log("incremented state counter");
-    
-    console.log("after unsubscribing and incrementing state counter", store.getState().counter);
-    console.log("subscriber didn't fire");    
+    setupEventListener(addBtn, "click", ()=> store.dispatch(actions.add()));
+    setupEventListener(subtractBtn, "click", ()=> store.dispatch(actions.subtract()));
+    setupEventListener(resetBtn, "click", ()=> store.dispatch(actions.reset()));
+    setupEventListener(removeSubscribeBtn, "click", ()=> unSubscribe());
 }
 
 main();
